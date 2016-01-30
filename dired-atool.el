46d936febb99ae3b7bde87f98176df6fe456081c
HKey <s06139@gmail.com>
2016-01-30 21:48:45 +0900

    Add commentary section

--- dired-atool.el
+++ dired-atool.el
@@ -23,7 +23,10 @@
 
 ;;; Commentary:
 
-;;
+;; Dired-atool is an utility to pack/unpack files with atool on dired.
+
+;; Please see https://github.com/HKey/dired-atool/blob/master/README.md
+;; for more details.
 
 ;;; Code:
 
